cbf11aac9b5d5a9bcd4bf6f12973ba2e58297deb
MinJoon Kim <kmj73203@naver.com>
2023-02-25 11:14:11 +0900

    프로젝트 생성 및 초기 설정

---
+++ Source/C_Portfolio/C_PortfolioGameModeBase.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "C_PortfolioGameModeBase.generated.h"
+
+UCLASS()
+class C_PORTFOLIO_API AC_PortfolioGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
